ca612eed93d0363184669710beab6183e6ed08b6
gfrangi-66 <gfrangi@me.com>
2026-04-19 15:24:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', //
+export default defineConfig(({ mode }) => {
+  // Carica le variabili d'ambiente (quelle locali o quelle di GitHub Actions)
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    base: './',
+    define: {
+      // Forza Vite a riconoscere le variabili anche se il sistema fatica a passarle
+      'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+      'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
+    },
+  };
 });
